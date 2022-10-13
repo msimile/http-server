17b6f9c717aa471a9bb6dfa1c154deb47322767e
Marco Simile <104313781+msimile@users.noreply.github.com>
2022-10-13 20:38:26 +0200

    config prettier

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,18 @@
-type Name = string;
-
-const nome: Name = "Marco";
-
-throw new Error("Error!");
+const nome: string = "Marco";
 
 console.log(`Hello ${nome}`);
+
+// import express from "express";
+// import "express-async-errors";
+
+// const app = express();
+
+// app.get("/", (request, response) => {
+//   response.send("Up and running!");
+// });
+
+// const port = 3000;
+
+// app.listen(port, () => {
+//   console.log(`[server]: Server is running at http://localhost:${port}`);
+// });
