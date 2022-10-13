f9890f8a2a032809c8022e2e35ee776d19a26a6d
Marco Simile <104313781+msimile@users.noreply.github.com>
2022-10-13 22:30:01 +0200

    create app.ts

--- dist/server.js
+++ dist/server.js
@@ -3,14 +3,9 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = __importDefault(require("express"));
-require("express-async-errors");
-const app = (0, express_1.default)();
-app.get("/", (request, response) => {
-    response.send("This is the Space Facts API");
-});
+const app_1 = __importDefault(require("./app"));
 const port = 3000;
-app.listen(port, () => {
+app_1.default.listen(port, () => {
     console.log(`[server]: Server is running at http://localhost:${port}`);
 });
 //# sourceMappingURL=server.js.map
